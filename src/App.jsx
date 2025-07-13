7b7915383a4a1aa54537aa28b1dbea617144f47b
joaomartinscode <jfbrancomartins@gmail.com>
2025-07-14 00:19:40 +0100

    Set up Routing in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Shop from "./pages/Shop";
+
 function App() {
 	return (
-		<>
-			<h1>Welcome to my shop APP!</h1>
-		</>
+		<Router>
+			<nav style={{ padding: "1rem", backgroundColor: "#f0f0f0" }}>
+				<Link to="/" style={{ marginRight: "1rem" }}>Home</Link>
+				<Link to="/shop">Shop</Link>
+			</nav>
+
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/shop" element={<Shop />} />
+			</Routes>
+		</Router>
 	);
 }
 
