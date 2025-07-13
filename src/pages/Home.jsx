65e91499617c1fedf1db164ece345dbdf1ab852e
joaomartinscode <jfbrancomartins@gmail.com>
2025-07-14 00:15:17 +0100

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+	return (
+		<div className="home">
+			<h1>Welcome to the Shopping Cart</h1>
+			<p>Your one-stop shop for all your needs!</p>
+		</div>
+	);
+}
+
+export default Home;
---
+++ src/pages/Shop.jsx
@@ -0,0 +1,10 @@
+function Shop() {
+	return (
+		<div className="shop">
+			<h1>Welcome to the Shop</h1>
+			<p>Here you'll find our products</p>
+		</div>
+	);
+}
+
+export default Shop;
